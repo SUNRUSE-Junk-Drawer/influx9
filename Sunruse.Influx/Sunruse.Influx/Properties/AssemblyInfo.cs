5c586f06dd3ea439cca77d4e806bd6985e7f3332
jameswilddev <jameswilddev@outlook.com>
2017-06-22 21:01:38 +0100

    Added an empty project.

---
+++ Sunruse.Influx/Sunruse.Influx/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sunruse.Influx")]
+[assembly: AssemblyDescription("Akka.NET actors and associated types for the Sunruse.Influx toolchain.")]
+[assembly: AssemblyCompany("Sunruse")]
+[assembly: AssemblyProduct("Sunruse.Influx")]
+[assembly: AssemblyCopyright("Copyright © Sunruse 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8bdfbed7-4e5a-410a-9692-e288d3ff7c91")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
