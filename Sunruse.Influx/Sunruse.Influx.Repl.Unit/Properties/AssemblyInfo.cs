54f8220f3f332b5ae7171f69abe28765bbeb461c
jameswilddev <jameswilddev@outlook.com>
2017-06-22 22:49:18 +0100

    Added a unit test project for the REPL CLI.

---
+++ Sunruse.Influx/Sunruse.Influx.Repl.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sunruse.Influx.Repl.Unit")]
+[assembly: AssemblyDescription("Unit tests for Sunruse.Influx.Repl.")]
+[assembly: AssemblyCompany("Sunruse")]
+[assembly: AssemblyProduct("Sunruse.Influx")]
+[assembly: AssemblyCopyright("Copyright © Sunruse 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d0f298b7-40ff-44d0-b737-997972e27cad")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
