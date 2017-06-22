f1d694aebdbcaa6d348b31e2375d865139c827cc
jameswilddev <jameswilddev@outlook.com>
2017-06-22 21:08:34 +0100

    Added a blank REPL project.

---
+++ Sunruse.Influx/Sunruse.Influx.Repl/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sunruse.Influx.Repl")]
+[assembly: AssemblyDescription("A console app providing a REPL CLI (Read-Evaluate-Print-Loop Command-Line-Interface) to the compiler.")]
+[assembly: AssemblyCompany("Sunruse")]
+[assembly: AssemblyProduct("Sunruse.Influx")]
+[assembly: AssemblyCopyright("Copyright © Sunruse 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8c6ed272-0206-4f5f-8fa3-c8acdad1e990")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
