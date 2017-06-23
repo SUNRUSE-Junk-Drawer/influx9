44983efd89dc1aadd1042c81315598b67259e122
jameswilddev <jameswilddev@outlook.com>
2017-06-23 22:45:38 +0100

    Added a test project for Sunruse.Influx.

---
+++ Sunruse.Influx/Sunruse.Influx.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sunruse.Influx.Unit")]
+[assembly: AssemblyDescription("Unit tests for Sunruse.Influx.")]
+[assembly: AssemblyCompany("Sunruse")]
+[assembly: AssemblyProduct("Sunruse.Influx")]
+[assembly: AssemblyCopyright("Copyright © Sunruse 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1d31a680-72e2-4446-9370-24da07ce165b")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
