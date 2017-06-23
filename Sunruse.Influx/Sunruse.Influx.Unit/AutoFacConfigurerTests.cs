7223116164381a0117e1925bbccc9cb0288eb491
jameswilddev <jameswilddev@outlook.com>
2017-06-23 23:47:06 +0100

    Added a static method to configure an AutoFac ContainerBuilder.

---
+++ Sunruse.Influx/Sunruse.Influx.Unit/AutoFacConfigurerTests.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using System;
+using Xunit;
+
+namespace Sunruse.Influx.Unit
+{
+    public sealed class AutoFacConfigurerTests
+    {
+        [Fact]
+        public void RegistersWhiteSpaceSplitter()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            AutoFacConfigurer.Configure(containerBuilder);
+
+            Assert.True(containerBuilder.Build().IsRegistered<WhiteSpaceSplitter>());
+        }
+    }
+}
---
+++ Sunruse.Influx/Sunruse.Influx/AutoFacConfigurer.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunruse.Influx
+{
+    /// <summary>Configures a <see cref="ContainerBuilder"/> to use types from this assembly.</summary>
+    public static class AutoFacConfigurer
+    {
+        /// <summary>Configures a <see cref="ContainerBuilder"/> to use types from this assembly.</summary>
+        /// <param name="containerBuilder">The <see cref="ContainerBuilder"/> to configure.</param>
+        public static void Configure(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<WhiteSpaceSplitter>();
+        }
+    }
+}
