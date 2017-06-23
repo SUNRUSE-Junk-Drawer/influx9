1abf073f980cf8270185f9827873bdcff638c32b
jameswilddev <jameswilddev@outlook.com>
2017-06-23 22:49:16 +0100

    Added a base class to Sunruse.Influx.Unit which configures the test system to ensure that all messages and constructors are serializable.

---
+++ Sunruse.Influx/Sunruse.Influx.Unit/ConfiguredTestKit.cs
@@ -0,0 +1,21 @@
+﻿using Akka.TestKit.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunruse.Influx.Unit
+{
+    public sealed class ConfiguredTestKit : TestKit
+    {
+        public ConfiguredTestKit() : base(@"
+            akka {
+                actor {
+                    serialize-messages = on
+                    serialize-creators = on
+                }
+            }
+        ") { }
+    }
+}
