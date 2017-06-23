7361ada5bf81784c07361ad9f9e4d17d93d4bc2e
jameswilddev <jameswilddev@outlook.com>
2017-06-23 22:58:50 +0100

    Added an actor to split whitespace into untyped tokens.

---
+++ Sunruse.Influx/Sunruse.Influx.Unit/WhiteSpaceSplitterTests.cs
@@ -0,0 +1,133 @@
+﻿using Akka.TestKit.Xunit2;
+using Akka.Actor;
+using Xunit;
+using System.Linq;
+using System;
+
+namespace Sunruse.Influx.Unit
+{
+    public sealed class WhiteSpaceSplitterTests : ConfiguredTestKit
+    {
+        [Fact]
+        public void ReturnsEmptyForEmpty()
+        {
+            var actor = ActorOf<WhiteSpaceSplitter>();
+
+            actor.Tell(new SplitSourceByWhiteSpace(""));
+
+            var response = ExpectMsgFrom<SourceSplitByWhiteSpace>(actor);
+            ExpectNoMsg();
+            Assert.Empty(response.Tokens);
+        }
+
+        [Fact]
+        public void FindsASingleToken()
+        {
+            var actor = ActorOf<WhiteSpaceSplitter>();
+
+            actor.Tell(new SplitSourceByWhiteSpace("testToken2378$"));
+
+            var response = ExpectMsgFrom<SourceSplitByWhiteSpace>(actor);
+            ExpectNoMsg();
+            var responseTokens = response.Tokens.ToList();
+            Assert.Equal(1, responseTokens.Count);
+            Assert.Equal("testToken2378$", responseTokens[0].Text);
+            Assert.Equal(0, responseTokens[0].Origin.StartIndex);
+            Assert.Equal(13, responseTokens[0].Origin.EndIndex);
+        }
+
+        [Fact]
+        public void FindsASingleTokenPrecededByWhiteSpace()
+        {
+            var actor = ActorOf<WhiteSpaceSplitter>();
+
+            actor.Tell(new SplitSourceByWhiteSpace("   \n   \r \t  testToken2378$"));
+
+            var response = ExpectMsgFrom<SourceSplitByWhiteSpace>(actor);
+            ExpectNoMsg();
+            var responseTokens = response.Tokens.ToList();
+            Assert.Equal(1, responseTokens.Count);
+            Assert.Equal("testToken2378$", responseTokens[0].Text);
+            Assert.Equal(12, responseTokens[0].Origin.StartIndex);
+            Assert.Equal(25, responseTokens[0].Origin.EndIndex);
+        }
+
+        [Fact]
+        public void FindsASingleTokenFollowedByWhiteSpace()
+        {
+            var actor = ActorOf<WhiteSpaceSplitter>();
+
+            actor.Tell(new SplitSourceByWhiteSpace("testToken2378$   \n   \r \t  "));
+
+            var response = ExpectMsgFrom<SourceSplitByWhiteSpace>(actor);
+            ExpectNoMsg();
+            var responseTokens = response.Tokens.ToList();
+            Assert.Equal(1, responseTokens.Count);
+            Assert.Equal("testToken2378$", responseTokens[0].Text);
+            Assert.Equal(0, responseTokens[0].Origin.StartIndex);
+            Assert.Equal(13, responseTokens[0].Origin.EndIndex);
+        }
+
+        [Fact]
+        public void FindsMultipleTokens()
+        {
+            var actor = ActorOf<WhiteSpaceSplitter>();
+
+            actor.Tell(new SplitSourceByWhiteSpace("testToken2378$  \n \t \r SOMEmoretokens 2furtherTOKEN   \n \r \t followingNewlinesToken \n \r \t WithMORE!after \n \t \r andMoreHere"));
+
+            var response = ExpectMsgFrom<SourceSplitByWhiteSpace>(actor);
+            ExpectNoMsg();
+            var responseTokens = response.Tokens.ToList();
+            Assert.Equal(6, responseTokens.Count);
+            Assert.Equal("testToken2378$", responseTokens[0].Text);
+            Assert.Equal(0, responseTokens[0].Origin.StartIndex);
+            Assert.Equal(13, responseTokens[0].Origin.EndIndex);
+            Assert.Equal("SOMEmoretokens", responseTokens[1].Text);
+            Assert.Equal(22, responseTokens[1].Origin.StartIndex);
+            Assert.Equal(35, responseTokens[1].Origin.EndIndex);
+            Assert.Equal("2furtherTOKEN", responseTokens[2].Text);
+            Assert.Equal(37, responseTokens[2].Origin.StartIndex);
+            Assert.Equal(49, responseTokens[2].Origin.EndIndex);
+            Assert.Equal("followingNewlinesToken", responseTokens[3].Text);
+            Assert.Equal(59, responseTokens[3].Origin.StartIndex);
+            Assert.Equal(80, responseTokens[3].Origin.EndIndex);
+            Assert.Equal("WithMORE!after", responseTokens[4].Text);
+            Assert.Equal(88, responseTokens[4].Origin.StartIndex);
+            Assert.Equal(101, responseTokens[4].Origin.EndIndex);
+            Assert.Equal("andMoreHere", responseTokens[5].Text);
+            Assert.Equal(109, responseTokens[5].Origin.StartIndex);
+            Assert.Equal(119, responseTokens[5].Origin.EndIndex);
+        }
+
+        [Fact]
+        public void FindsMultipleTokensWithinWhiteSpace()
+        {
+            var actor = ActorOf<WhiteSpaceSplitter>();
+
+            actor.Tell(new SplitSourceByWhiteSpace(" \n \r \t testToken2378$  \n \t \r SOMEmoretokens 2furtherTOKEN   \n \r \t followingNewlinesToken \n \r \t WithMORE!after \n \t \r andMoreHere \r \n \n \t "));
+
+            var response = ExpectMsgFrom<SourceSplitByWhiteSpace>(actor);
+            ExpectNoMsg();
+            var responseTokens = response.Tokens.ToList();
+            Assert.Equal(6, responseTokens.Count);
+            Assert.Equal("testToken2378$", responseTokens[0].Text);
+            Assert.Equal(7, responseTokens[0].Origin.StartIndex);
+            Assert.Equal(20, responseTokens[0].Origin.EndIndex);
+            Assert.Equal("SOMEmoretokens", responseTokens[1].Text);
+            Assert.Equal(29, responseTokens[1].Origin.StartIndex);
+            Assert.Equal(42, responseTokens[1].Origin.EndIndex);
+            Assert.Equal("2furtherTOKEN", responseTokens[2].Text);
+            Assert.Equal(44, responseTokens[2].Origin.StartIndex);
+            Assert.Equal(56, responseTokens[2].Origin.EndIndex);
+            Assert.Equal("followingNewlinesToken", responseTokens[3].Text);
+            Assert.Equal(66, responseTokens[3].Origin.StartIndex);
+            Assert.Equal(87, responseTokens[3].Origin.EndIndex);
+            Assert.Equal("WithMORE!after", responseTokens[4].Text);
+            Assert.Equal(95, responseTokens[4].Origin.StartIndex);
+            Assert.Equal(108, responseTokens[4].Origin.EndIndex);
+            Assert.Equal("andMoreHere", responseTokens[5].Text);
+            Assert.Equal(116, responseTokens[5].Origin.StartIndex);
+            Assert.Equal(126, responseTokens[5].Origin.EndIndex);
+        }
+    }
+}
---
+++ Sunruse.Influx/Sunruse.Influx/WhiteSpaceSplitter.cs
@@ -0,0 +1,104 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunruse.Influx
+{
+    /// <summary>Splits given source code by white-space into tokens.</summary>
+    public sealed class WhiteSpaceSplitter : ReceiveActor
+    {
+        /// <inheritdoc />
+        public WhiteSpaceSplitter()
+        {
+            Receive<SplitSourceByWhiteSpace>(ssbw =>
+            {
+                var tokens = new List<UntypedToken>();
+                string token = null;
+                for (var i = 0; i < ssbw.Source.Length; i++)
+                {
+                    var character = ssbw.Source[i];
+                    if (Char.IsWhiteSpace(character))
+                    {
+                        if (token != null)
+                        {
+                            tokens.Add(new UntypedToken(token, new Origin(i - token.Length, i - 1)));
+                            token = null;
+                        }
+                    }
+                    else token = (token ?? "") + character;
+                }
+                if (token != null) tokens.Add(new UntypedToken(token, new Origin(ssbw.Source.Length - token.Length, ssbw.Source.Length - 1)));
+                    Sender.Tell(new SourceSplitByWhiteSpace(tokens));
+            });
+        }
+    }
+
+    /// <summary>A request to <see cref="WhiteSpaceSplitter"/> to split source code by white-space.</summary>
+    public sealed class SplitSourceByWhiteSpace
+    {
+        /// <summary>The source code to split by white-space.</summary>
+        public readonly string Source;
+
+        /// <inheritdoc />
+        /// <param name="source"><see cref="Source"/>.</param>
+        public SplitSourceByWhiteSpace(string source)
+        {
+            Source = source;
+        }
+    }
+
+    /// <summary>Returned from <see cref="WhiteSpaceSplitter"/> in response to <see cref="SplitSourceByWhiteSpace"/>.</summary>
+    public sealed class SourceSplitByWhiteSpace
+    {
+        /// <summary>The <see cref="UntypedToken"/>s found by splitting <see cref="SplitSourceByWhiteSpace.Source"/>.</summary>
+        public readonly IEnumerable<UntypedToken> Tokens;
+
+        /// <inheritdoc />
+        /// <param name="tokens"><see cref="Tokens"/>.</param>
+        public SourceSplitByWhiteSpace(IEnumerable<UntypedToken> tokens)
+        {
+            Tokens = tokens;
+        }
+    }
+
+    /// <summary>Describes where in the source code something came from.</summary>
+    public sealed class Origin
+    {
+        /// <summary>The inclusive earliest index <see langword="this"/> was defined in.</summary>
+        public readonly int StartIndex;
+
+        /// <summary>The inclusive latest index <see langword="this"/> was defined in.</summary>
+        public readonly int EndIndex;
+
+        /// <inheritdoc />
+        /// <param name="startIndex"><see cref="StartIndex"/>.</param>
+        /// <param name="endIndex"><see cref="EndIndex"/>.</param>
+        public Origin(int startIndex, int endIndex)
+        {
+            StartIndex = startIndex;
+            EndIndex = endIndex;
+        }
+    }
+
+    /// <summary>Represents a token which has been split from source code by <see cref="WhiteSpaceSplitter"/>.</summary>
+    public sealed class UntypedToken
+    {
+        /// <summary>The <see cref="string"/> split from source code.</summary>
+        public readonly string Text;
+
+        /// <summary>The <see cref="Origin"/> of <see cref="Text"/>.</summary>
+        public readonly Origin Origin;
+
+        /// <inheritdoc />
+        /// <param name="text"><see cref="Text"/>.</param>
+        /// <param name="origin"><see cref="Origin"/>.</param>
+        public UntypedToken(string text, Origin origin)
+        {
+            Text = text;
+            Origin = origin;
+        }
+    }
+}
