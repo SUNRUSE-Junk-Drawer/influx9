1f4604004bc69a15e402d3b5677dde3e96baae61
jameswilddev <jameswilddev@outlook.com>
2017-06-23 23:52:17 +0100

    Added an AutoFacConfigurer for Sunruse.Influx.Repl.

---
+++ Sunruse.Influx/Sunruse.Influx.Repl.Unit/AutoFacConfigurerTests.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using System;
+using Xunit;
+
+namespace Sunruse.Influx.Repl.Unit
+{
+    public sealed class AutoFacConfigurerTests
+    {
+        [Fact]
+        public void RegistersConsoleReader()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            AutoFacConfigurer.Configure(containerBuilder);
+
+            Assert.True(containerBuilder.Build().IsRegistered<ConsoleReader>());
+        }
+
+        [Fact]
+        public void RegistersConsoleWriter()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            AutoFacConfigurer.Configure(containerBuilder);
+
+            Assert.True(containerBuilder.Build().IsRegistered<ConsoleWriter>());
+        }
+    }
+}
---
+++ Sunruse.Influx/Sunruse.Influx.Repl/AutoFacConfigurer.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunruse.Influx.Repl
+{
+    /// <summary>Configures a <see cref="ContainerBuilder"/> to use types from this assembly.</summary>
+    public static class AutoFacConfigurer
+    {
+        /// <summary>Configures a <see cref="ContainerBuilder"/> to use types from this assembly.</summary>
+        /// <param name="containerBuilder">The <see cref="ContainerBuilder"/> to configure.</param>
+        public static void Configure(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<ConsoleReader>();
+            containerBuilder.RegisterType<ConsoleWriter>();
+        }
+    }
+}
